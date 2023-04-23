b2923a22f734d860a25b9733a62503243620dfce
AlkalineDog23 <117673407+AlkalineDog23@users.noreply.github.com>
2023-04-23 16:29:26 -0400

    Added Different menus
    Added different menus and added levelmanagers worked on mostly design and some scripting

---
+++ Assets/Hour 14 Package/QuitGame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+   //Quit Game
+   public void Quit()
+    {
+        Application.Quit();
+    }
+}
