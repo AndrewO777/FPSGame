61b9a4b46b0b4e01db0d0238a31a7fc492a3cf29
Adam0240 <adam.adusky@gmail.com>
2023-04-27 18:28:03 -0400

    fixed gun sync

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -15,12 +15,22 @@ public class Weapon : NetworkBehaviour
     public int damage;
 
     private float currentCooldown;
-    private int currentIndex;
+    //private int currentIndex;
     private GameObject currentWeapon;
 
-    void Start()
+    private NetworkVariable<int> currentIndex = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    private void Start()
     {
-        //Creating pistol, rework this later.
+        currentIndex.OnValueChanged += OnWeaponIndexChanged;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            Equip(currentIndex.Value);
+        }
     }
 
     void Update()
@@ -31,21 +41,22 @@ public class Weapon : NetworkBehaviour
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
-            Equip(0);
+            EquipAndSync(0);
         else if (Input.GetKeyDown(KeyCode.Alpha2))
-            Equip(1);
+            EquipAndSync(1);
         else if (Input.GetKeyDown(KeyCode.Alpha3))
-            Equip(2);
-        if (currentWeapon != null)
+            EquipAndSync(2);
         {
             Aim(Input.GetMouseButton(1));
-            if (loadout[currentIndex].burst != 1){
+            if (loadout[currentIndex.Value].burst != 1)
+            {
                 if (Input.GetMouseButtonDown(0) && currentCooldown <= 0)
                 {
                     Shoot();
                 }
             }
-            else {
+            else
+            {
                 if (Input.GetMouseButton(0) && currentCooldown <= 0)
                 {
                     Shoot();
@@ -54,11 +65,41 @@ public class Weapon : NetworkBehaviour
             //elasticity
             currentWeapon.transform.localPosition = Vector3.Lerp(currentWeapon.transform.localPosition, Vector3.zero, Time.deltaTime * 4f);
             //firerate
-            if(currentCooldown > 0)
+            if (currentCooldown > 0)
             {
                 currentCooldown -= Time.deltaTime;
             }
         }
+    }
+
+    void EquipAndSync(int index)
+{
+    currentIndex.Value = index;
+    Equip(index);
+    EquipClientRpc(index);
+}
+
+    [ClientRpc]
+    void EquipClientRpc(int index)
+    {
+        if (IsOwner)
+        {
+            return;
+        }
+
+        Equip(index);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    void RequestEquipServerRpc(int index, ServerRpcParams rpcParams = default)
+    {
+        currentIndex.Value = index;
+        Equip(index);
+    }
+
+    void OnWeaponIndexChanged(int oldValue, int newValue)
+    {
+        Equip(newValue);
     }
 
     void Equip(int index)
@@ -67,8 +108,9 @@ public class Weapon : NetworkBehaviour
         {
             Destroy(currentWeapon);
         }
-        currentIndex = index;
-        GameObject newEquipment = Instantiate(loadout[index].prefab, weaponParent.position, weaponParent.rotation, weaponParent) as GameObject;
+        currentIndex.Value = index;
+        GameObject newEquipment = Instantiate(loadout[index].prefab, weaponParent.position, weaponParent.rotation) as GameObject;
+        newEquipment.transform.SetParent(weaponParent);
         newEquipment.transform.localPosition = Vector3.zero;
         newEquipment.transform.localEulerAngles = Vector3.zero;
         currentWeapon = newEquipment;
@@ -82,11 +124,11 @@ public class Weapon : NetworkBehaviour
         Transform state_hip = currentWeapon.transform.Find("States/Hip");
         if (isAiming)
         {
-            anchor.position = Vector3.Lerp(anchor.position, state_ads.position, Time.deltaTime * loadout[currentIndex].aimSpeed);
+            anchor.position = Vector3.Lerp(anchor.position, state_ads.position, Time.deltaTime * loadout[currentIndex.Value].aimSpeed);
         }
         else
         {
-            anchor.position = Vector3.Lerp(anchor.position, state_hip.position, Time.deltaTime * loadout[currentIndex].aimSpeed);
+            anchor.position = Vector3.Lerp(anchor.position, state_hip.position, Time.deltaTime * loadout[currentIndex.Value].aimSpeed);
         }
     }
     void Shoot()
@@ -94,45 +136,48 @@ public class Weapon : NetworkBehaviour
         Transform spawn = transform.Find("Main Camera");
         RaycastHit hit = new RaycastHit();
         int shots = 1;
-        if (currentIndex == 1)
+        if (currentIndex.Value == 1)
             shots = 8;
-        for (;shots > 0;--shots){
-        //bloom
-        Vector3 bloom = spawn.position + spawn.forward * 1000f;
-        bloom += Random.Range(-loadout[currentIndex].bloom, loadout[currentIndex].bloom) * spawn.up;
-        bloom += Random.Range(-loadout[currentIndex].bloom, loadout[currentIndex].bloom) * spawn.right;
-        bloom -= spawn.position;
-        bloom.Normalize();
-        //raycast
-        if (Physics.Raycast(spawn.position, bloom * 1000f, out hit, 1000f, canBeShot))
+        for (; shots > 0; --shots)
         {
-            //Add a if statement and make this the else for if you don't hit a player
-            if (hit.collider.gameObject.layer == 6)
+            //bloom
+            Vector3 bloom = spawn.position + spawn.forward * 1000f;
+            bloom += Random.Range(-loadout[currentIndex.Value].bloom, loadout[currentIndex.Value].bloom) * spawn.up;
+            bloom += Random.Range(-loadout[currentIndex.Value].bloom, loadout[currentIndex.Value].bloom) * spawn.right;
+            bloom -= spawn.position;
+            bloom.Normalize();
+            //raycast
+            if (Physics.Raycast(spawn.position, bloom * 1000f, out hit, 1000f, canBeShot))
             {
-                ParticleSystem bulletExp = Instantiate(explodePrefab, hit.point, Quaternion.identity);
-                bulletExp.transform.rotation = Quaternion.LookRotation((spawn.transform.position - hit.point).normalized);
-                bulletExp.Play();
-                Destroy(bulletExp, bulletExp.main.duration - 0.1f);
-                PlayerMovement player = hit.collider.GetComponent<PlayerMovement>();
-                if (player != null){
-                    player.TakeDamageServerRpc(player.myID, damage);
+                // Add a if statement and make this the else for if you don't hit a player
+                if (hit.collider.gameObject.layer == 6)
+                {
+                    ParticleSystem bulletExp = Instantiate(explodePrefab, hit.point, Quaternion.identity);
+                    bulletExp.transform.rotation = Quaternion.LookRotation((spawn.transform.position - hit.point).normalized);
+                    bulletExp.Play();
+                    Destroy(bulletExp, bulletExp.main.duration - 0.1f);
+                    PlayerMovement player = hit.collider.GetComponent<PlayerMovement>();
+                    if (player != null)
+                    {
+                        player.TakeDamageServerRpc(player.myID, damage);
+                    }
+                }
+                else
+                {
+                    ParticleSystem bulletExp = Instantiate(bulletEffectPrefab, hit.point, Quaternion.identity);
+                    bulletExp.transform.rotation = Quaternion.LookRotation((spawn.transform.position - hit.point).normalized);
+                    bulletExp.Play();
+                    Destroy(bulletExp, bulletExp.main.duration - 0.1f);
+                    GameObject newHole = Instantiate(bulletholePrefab, hit.point + hit.normal * 0.001f, Quaternion.identity) as GameObject;
+                    newHole.transform.LookAt(hit.point + hit.normal);
+                    Destroy(newHole, 5f);
                 }
             }
-            else{
-            ParticleSystem bulletExp = Instantiate(bulletEffectPrefab, hit.point, Quaternion.identity);
-            bulletExp.transform.rotation = Quaternion.LookRotation((spawn.transform.position - hit.point).normalized);
-            bulletExp.Play();
-            Destroy(bulletExp, bulletExp.main.duration - 0.1f);
-            GameObject newHole = Instantiate(bulletholePrefab, hit.point + hit.normal * 0.001f, Quaternion.identity) as GameObject;
-            newHole.transform.LookAt(hit.point + hit.normal);
-            Destroy(newHole, 5f);
-            }
-        }
         }
         //gun effects
-        currentWeapon.transform.Rotate(-loadout[currentIndex].recoil, 0, 0);
-        currentWeapon.transform.position -= currentWeapon.transform.forward * loadout[currentIndex].kickback;
+        currentWeapon.transform.Rotate(-loadout[currentIndex.Value].recoil, 0, 0);
+        currentWeapon.transform.position -= currentWeapon.transform.forward * loadout[currentIndex.Value].kickback;
         //firerate
-        currentCooldown = loadout[currentIndex].firerate;
+        currentCooldown = loadout[currentIndex.Value].firerate;
     }
 }
